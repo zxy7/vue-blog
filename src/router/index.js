dc20b0336551280ec160134edd4e0db95a962813
昝晓玉 <740410570@qq.com>
2018-03-30 10:50:06 +0800

    save

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
@@ -8,6 +9,26 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/dairy',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/about',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/tags',
       name: 'HelloWorld',
       component: HelloWorld
     }
