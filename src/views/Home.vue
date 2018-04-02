cf102327b2d3e7d61d4cac2661276819d26424e5
昝晓玉 <740410570@qq.com>
2018-04-02 20:00:52 +0800

    save

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="hello">
-  <Header> </Header>
-  <PostItem> </PostItem>
+    <Header> </Header>
+    <div class="postList"  v-for="item in postList" :key="item.id">
+
+      <PostItem :title="item.title" :subtitle="item.subtitle" :preview="item.preview" :meta="item.meta"> </PostItem>
+      <hr/>
+    </div>
 
   </div>
 </template>
@@ -9,12 +13,27 @@
 <script>
 import Header from '@/components/Header'
 import PostItem from '@/components/PostItem'
+// 使用 Mock
+var Mock = require('mockjs')
+var Random = Mock.Random
+var data = Mock.mock({
+  'list|1-4': [{
+    'id|+1': 1,
+    'title': Random.title(3, 5),
+    'subtitle': Random.title(6, 15),
+    'preview': Random.cparagraph(),
+    'meta': Random.date('M  d, yyyy')
+  }]
+})
+// 输出结果
+console.log(JSON.stringify(data, null, 4))
 export default {
   name: 'Home',
   components: { Header, PostItem },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      postList: data.list
     }
   }
 }
@@ -22,5 +41,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.postList{
+  padding: 0 16px;
+}
 </style>
